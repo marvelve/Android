865561dddfcbf099c929e1796467f8f3eacee429
MaryselaVelasco <marysela.velasco@gmail.com>
2024-11-19 10:41:45 -0500

    Android_V1

---
+++ src/appinventor/ai_marysela_velasco/Registro_APP/Login.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"230","Source":"Form","Properties":{"$Name":"Login","$Type":"Form","$Version":"31","ActionBar":"True","AppName":"App_Registro","Title":"Login","Uuid":"0","$Components":[{"$Name":"Label1","$Type":"Label","$Version":"5","FontBold":"True","FontSize":"16","Width":"-1050","Text":"Registro Exitoso","TextAlignment":"1","Uuid":"-2021609877"}]}}
+|#
